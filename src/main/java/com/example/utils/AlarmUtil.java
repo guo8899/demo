a8dec5098c75e6559cf025200acb1820611f582e
dearfuture <dearfuture2012@gmail.com>
2017-09-15 18:23:18 +0800

    for test

--- src/main/java/com/example/utils/AlarmUtil.java
+++ src/main/java/com/example/utils/AlarmUtil.java
@@ -1,8 +1,12 @@
 package com.example.utils;
 
+import com.example.entity.Alarm;
+
 /**
  * Created by fzy on 2017/9/15.
  */
 public class AlarmUtil {
-
+    public Alarm createFirst() {
+        return new Alarm();
+    }
 }
