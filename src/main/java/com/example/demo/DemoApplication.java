28e787c29ebf77c83a4f0b00ef1d81edeed6bdb5
dearfuture <dearfuture2012@gmail.com>
2017-09-21 16:05:31 +0800

    打包成war，支持内置tomcat运行和放在独立tomcat服务器运行

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,15 @@ package com.example.demo;
 import com.example.controller.AlarmController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class DemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AlarmController.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AlarmController.class, args);
 	}
