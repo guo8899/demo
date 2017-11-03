6761a6ef096a0b60286b38a7c79a321799d8e3e6
dearfuture <dearfuture2012@gmail.com>
2017-11-03 11:06:00 +0800

    重构代码

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,11 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void test() {
+		System.out.println("Hello");
+	}
 }
